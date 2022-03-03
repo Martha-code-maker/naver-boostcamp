400ddc3ad981d4133b99bec9e0bb6d2862b5770d
martha kwak <uj525@naver.com>
2022-03-03 21:21:35 +0900

    first commit

--- week1_mission/Q3_1.py
+++
@@ -1,9 +0,0 @@
-score = [(100, 100), (95, 90), (55, 60), (75, 80), (70, 70)]      
-
-
-def get_avg(score):
-    avg = list(map(lambda x: (x[0] + x[1])/2 , score))
-    for l, s in enumerate(avg):
-        print(f'{l+1} 번, 평균 {s}')
-
-get_avg(score)
--- week1_mission/Q5_2.py
+++
@@ -1,6 +0,0 @@
-def find_string(inputs: str):
-    return "".join([i if not i.isdigit() else " " for i in inputs ]).split()
-
-inputs = "cat32dog16cow"
-string_list = find_string(inputs)
-print(string_list)
